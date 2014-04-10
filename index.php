534f024bdf88a68dc65861ec180ec96d82a26c43
gokudoabc <gokudoabc@gmail.com>
2014-04-10 19:17:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,69 @@
-﻿<?php include ('_includes/head.php');?> 
-<title>Serralheria Ferro Battuto | Home Serralheria</title> 
-</head>
-<body class="home blog"> 
-    <div id="wrp"> 
-        <div id="header"> 	
-            <div id="headbg"> 
-                <h1 class="main"><a href="http://www.ferrobattuto.com.br/">Serralheria Ferro Battuto | Home Serralheria</a></h1> 
-               <?php include ('_includes/menu.php');?> 
-            </div> 
-        </div>     
-            <div id="inner">           
-                <div id="home">          
-                    <div id="moldura">         
-                        <img src="_imgs/_base/moldura.jpg" title="Serralheria Ferro Battuto" alt="Serralheria Ferro Battuto"/>          
-                        <p>Serralheria Ferro Battuto -  Desde 1959 satisfazendo seus clientes.</p>                    
-                    <div id="flash">           
-                </div>         
-            </div>         
-        </div> 
-        </div>
-    </div>
-<?php include ('_includes/footer.php');?> 
+
+ <title>H4CK3D BY GOKUDO4BC!</title>
+<style id="jsbin-css">
+
+</style>
+<center><low><span style="text-shadow: white 0px 0px 20px;"><span style="color: white; font-family: Verdana, sans-serif;"><FONT color=white><CENTER><BIG><b>◘xXx◘ O Gigante Acordou ◘xXx◘
+<CENTER><BIG><BIG><BIG><PRE><B><I><BIG><FONT color=white>###<font color="white">H4<font color="white">cK3d <font color="white">By:###</CENTER></BIG></BIG></BIG> <font color="white"><center>@ ~ G0kUdO4BC ~ @
+<center>
+</style>
+<body bgcolor="G">
+<font color=#FFFFFF>
+ <body onload="type_text()" style="background-color: rgb(0, 0, 0); color: rgb(153, 0, 0); background-image: url(&quot;http://i66.servimg.com/u/f66/14/86/38/04/ground10.gif&quot;);" alink="#000000" link="#0000cc" vlink="#000066">
+ <center><img src="http://media.giphy.com/media/P8OFTdKiOLTvW/giphy.gif"
+<pre><strong><font color="Black" style="text-shadow: 5px 5px 10px  white">
+  
+    <html>
+
+</<body>
+<br>
+<script type="text/javascript">
+
+window.document.write ("<p id=\"foo\" style=\"font-weight:bold;\"></p>");
+
+  var text="<font size=4><font color=white>Seu Site Foi Invadido Por  G0kUdO4BC! <br/> <font color=white><br/>Este é Somente o Começo <br />! ¥ ! Até a Proxima ! ¥ ! <br/><br /><font color=white><br />[Lo<font color=gray>adi<font color=white>ng]:<br/>Nós somos Legião. Nós não perdoamos. Nós não esquecemos. Esperem por nós. <br/></font><font color=white><br/>[»] Brazil [«] Ant Corrupção [»] Anonymous [«] <br/><br /><font color=white><br />Exit!";
+
+ var speed=10;
+
+ var a=0;
+
+
+
+ function write_text ()
+
+  {
+
+   if (a<=text.length)
+
+    {
+
+     window.document.getElementById ("foo").innerHTML=text.substr (0, a++)+"_";
+
+     window.setTimeout ("write_text ()", speed);
+
+    }
+
+  }
+
+
+
+ window.onload=write_text;
+</script>
+<br>
+
+<script> alert("@ ~ G0kUdO4BC ~ @")</script>
+      <EMBED SRC="http://a.tumblr.com/tumblr_l5zn3zQJ4X1qaw44uo1.mp3" WIDTH="0" HEIGHT="0" AUTOSTART="TRUE" CONTROLS="SMALLCONSOLE" VOLUME="90"OOP="FALSE"></EMBED> 
+
+<script>
+
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1656750-34']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+</script>
